9a2f111b8cb908e43ca15162385817b91b3ba285
ILya <zakharuni13@gmail.com>
2015-02-23 19:02:22 +0400

    added search function

--- firstProject++/main.cpp
+++ firstProject++/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int Pi(string s){
+    
     unsigned long length = s.length();
     int j = 1;
     int i;
@@ -22,6 +23,34 @@ int Pi(string s){
     return result;
 }
 
+int piSearch(string password, string decryptPass){
+    
+    int i = 0;
+    int j = 0;
+    int shift = 0;
+    string s;
+    unsigned long passLength = password.length();
+    unsigned long dPassLength = decryptPass.length();
+    
+    while(j < dPassLength && i < passLength){
+        
+        if(password[i] == decryptPass[j]){
+            i++;j++; s.append(&password[i]);
+        }
+        else{
+            j -= Pi(s);
+            s = "";
+            shift += i;
+        }
+        
+    }
+    
+    if(j == dPassLength)
+        return shift;
+    
+    return -1;
+}
+
 int main(int argc, const char * argv[])
 {
     int N;
@@ -31,6 +60,9 @@ int main(int argc, const char * argv[])
     cin >> N;
     cin >> password;
     cin >> decryptPass;
+    password += password;
+    
+    cout << piSearch(password, decryptPass) << endl;
     
     
     return 0;
